48aa2b31e5fb96f4b54d214cdb15a9b7bffdad08
Nerixyz <nero.9@hotmail.de>
2020-06-15 13:08:10 +0200

    feat(transport): Use TransformStream as a replacement for PassThrough

--- lib/client/transport/websocket-transport.ts
+++ lib/client/transport/websocket-transport.ts
@@ -4,25 +4,28 @@ import { ignoreErrors } from "../../utils/ignore-errors.ts";
 import { Transport } from "./transport.ts";
 
 export class WebSocketTransport implements Transport {
-  //@ts-ignore
-  public readable;
+  public readable: ReadableStream<string>;
+
+  private duplex: TransformStream<string, string>;
 
   private readonly config: ExpandedWebSocketTransportConfiguration;
   private wsStream?: WebSocket;
 
   public constructor(config: ExpandedWebSocketTransportConfiguration) {
     this.config = config;
+    this.duplex = new TransformStream();
+    this.readable = this.duplex.readable;
   }
   close(): void {
     if(this.wsStream && !this.wsStream.isClosed) {
       this.wsStream.close();
-      this.readable.cancel();
     }
   }
 
   public async connect(connectionListener?: () => void): Promise<void> {
     this.wsStream = await connectWebSocket(this.config.url);
-    this.readable = asyncIteratorToReadable(this.wsStream);
+
+    asyncIteratorToReadable(this.wsStream).pipeTo(this.duplex.writable);
     connectionListener?.();
   }
 
@@ -30,12 +33,21 @@ export class WebSocketTransport implements Transport {
     this.wsStream!.send(chunk);
   }
 }
-export function asyncIteratorToReadable<T>(iterable: AsyncIterable<T>): ReadableStream<T> {
+
+export function asyncIteratorToReadable(iterable: AsyncIterable<string | object>): ReadableStream<string> {
   const iterator = iterable[Symbol.asyncIterator]();
   return new ReadableStream({
     async pull(controller: ReadableStreamDefaultController) {
       iterator.next()
-        .then(result => result.done ? controller.close() : (controller.enqueue(result.value)))
+        .then(result => {
+          if(result.done) {
+            return controller.close();
+          } else if(typeof result.value === "string") {
+            return controller.enqueue(result.value);
+          } else {
+            console.log(result.value);
+          }
+        })
         .catch(e => controller.error(e));
     }
   });
